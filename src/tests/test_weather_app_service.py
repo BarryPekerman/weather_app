2631f9af49d81e6f9646ad85323d8b6bb358464d
barry.pekerman <barrypek@gmail.com>
2025-10-14 11:40:33 +0300

    moved hardcoded secrets to .env file

--- src/tests/test_weather_app_service.py
+++ src/tests/test_weather_app_service.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from ..app import create_app  # Relative import
 from ..services.weather_app_service import WeatherAppService  # Relative impor
 from ..utils.weatherutils import WeatherUtil, WeatherForecast, WeatherDay
+from ..utils.gpsutils import LocationData
 
 class TestIntegration(unittest.TestCase):
     def setUp(self):
@@ -13,13 +14,20 @@ class TestIntegration(unittest.TestCase):
 
     @patch('weather_app.services.weather_app_service.WeatherAppService.get_weather_forecast')
     def test_home_route(self, mock_get_weather_forecast):
-        # Mock the WeatherAppService to return a fake forecast
-        mock_get_weather_forecast.return_value = WeatherForecast({
+        # Mock the WeatherAppService to return a fake forecast and a location
+        fake_forecast = WeatherForecast({
             'date': ['2023-10-01'],
             'minimum_temperture': [15],
             'maximum_temperture': [25],
             'average_humidity': [65]
         }, days=1)
+        fake_location = LocationData({
+            'latitude': 0,
+            'longitude': 0,
+            'country': 'IL',
+            'name': 'Tel Aviv'
+        })
+        mock_get_weather_forecast.return_value = (fake_forecast, fake_location)
 
         # Simulate a POST request to the home route
         response = self.client.post('/', data={'locname': 'Tel Aviv', 'days': '1'})
@@ -29,8 +37,8 @@ class TestIntegration(unittest.TestCase):
 
     @patch('weather_app.services.weather_app_service.WeatherAppService.get_weather_forecast')
     def test_home_route_failure(self, mock_get_weather_forecast):
-        # Mock the WeatherAppService to return None (simulate failure)
-        mock_get_weather_forecast.return_value = None
+        # Mock the WeatherAppService to return (None, None) (simulate failure)
+        mock_get_weather_forecast.return_value = (None, None)
 
         # Simulate a POST request to the home route
         response = self.client.post('/', data={'locname': 'Invalid Location', 'days': '1'})
